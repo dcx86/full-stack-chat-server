0b7c602d87fc20c2fbb2dd5e2e3975992f2ab555
Ludvig Lindqvist <ludvig.lindqvist.rovira@gmail.com>
2019-12-19 11:56:10 +0100

    simple typescript soketio

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,25 @@
-import express from 'express'
+import * as express from 'express'
+import * as socketio from 'socket.io'
+import * as path from 'path'
 
 const app = express()
-const port = 3000
+app.set('port', process.env.PORT || 3000)
 
-const server = app.listen(port, () => {
-  console.log('server is running on port:', port)
+let http = require('http').Server(app)
+let io = require('socket.io')(http)
+
+app.get('/', (req: any, res: any) => {
+  res.sendFile(path.resolve('./client/index.html'))
+})
+
+io.on('connection', function(socket: any) {
+  console.log('a user connected')
+  socket.on('message', function(message: any) {
+    console.log(message)
+    socket.emit('message', message)
+  })
+})
+
+const server = http.listen(3000, function() {
+  console.log('server is running on port:', 3000)
 })
